7c6553f81ab0974af66edd3e70ed6c971a01ef2d
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-01-23 11:13:03 +0700

    manage token

--- token_gdrive/urls.py
+++ token_gdrive/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^new-token$', views.index, name='index'),
     url(r'^add-token$', views.add_token, name='add_token'),
+    url(r'^list-token$', views.list_token, name='list_token'),
+    url(r'^credential-action$', views.credential_action, name='credential_action'),
 ]
--- token_gdrive/views.py
+++ token_gdrive/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 import subprocess
+from django.views.decorators.csrf import csrf_exempt
 from token_gdrive.models import *
 from django.contrib.auth.decorators import login_required, permission_required
 from django.core.exceptions import PermissionDenied
@@ -24,3 +25,28 @@ def add_token(request):
         return HttpResponse(out.decode().split("\n")[1])
     else:
         return HttpResponse("no sp")
+
+@login_required
+def list_token(request):
+    credentials = Credentials.objects.all()
+    data = {"credentials": credentials}
+    return render(request, 'token_gdrive/list_token.html', data)
+
+@csrf_exempt
+@login_required
+@permission_required('token_gdrive.delete_credential', raise_exception=True)
+def credential_action(request):
+    try:
+        if request.method == 'POST':
+            action = request.POST.get('action', None)
+            if action == 'delete':
+                id = request.POST.get('id')
+                credential = Credentials.objects.get(pk=id)
+                credential.delete()
+            elif action == 'start':
+                pass
+
+            result = '{"status":"success", "msg": "%(action)s success"}'%{"action":action}
+    except Exception as e:
+        result = '{"status":"error", "msg": "%(action)s fail: %(error)s" }'%{"action":action, "error": str(e).replace("\n", " ").replace('"', '\\"')}
+    return HttpResponse(result)
